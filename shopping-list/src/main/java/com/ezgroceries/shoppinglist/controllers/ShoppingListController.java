99b2ed50c25db957ac4026887158280c3b7e397a
Wouter Stinckens <wouter.stinckens@kbc.be>
2019-09-30 11:22:37 +0200

    lab-02 Adding add cocktail to shopping list endpoint

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/controllers/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/controllers/ShoppingListController.java
@@ -1,10 +1,14 @@
 package com.ezgroceries.shoppinglist.controllers;
 
+import com.ezgroceries.shoppinglist.resources.CocktailResource;
 import com.ezgroceries.shoppinglist.resources.ShoppingListResource;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jd70695 - Wouter Stinckens
  * @since UOC201910
  */
-@RestController("/shopping-list")
+@RestController
+@RequestMapping("/shopping-lists")
 public class ShoppingListController {
 
     @PostMapping
@@ -21,4 +26,8 @@ public class ShoppingListController {
         return new ShoppingListResource(UUID.fromString("eb18bb7c-61f3-4c9f-981c-55b1b8ee8915"), newShoppingList.getName());
     }
 
+    @PostMapping("/{shoppingListId}/cocktails")
+    public List<CocktailResource> addCocktails(@PathVariable UUID shoppingListId, @RequestBody List<CocktailResource> cocktails) {
+        return cocktails;
+    }
 }
